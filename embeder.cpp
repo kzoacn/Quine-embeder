0939214980f6b0e3805ea99adb7c857ca9acd4c5
kzoacn <kzoacn@outlook.com>
2018-04-03 20:31:03 +0800

    fix for \ \%

--- embeder.cpp
+++ embeder.cpp
@@ -6,6 +6,8 @@ string fix(char c){
     if(c==9)return "%c";
     if(c==34)return "%c";
     if(c==39)return "%c";
+    if(c==37)return "%c";
+    if(c==92)return "%c";
     return string(1,c);
 }
 int main(){
@@ -23,13 +25,13 @@ int main(){
         cout<<fix(c);
     cout<<"int main()%c{%cfunction1();FILE *file=fopen(%ctmp%c,%cw%c);%cchar *s=%c%s%c;%cfprintf(file,s,";
     //cout << ASCII \t \n \"
-    for(auto c:content)if(c==10 || c==9 || c==34 || c==39)
+    for(auto c:content)if(c==10 || c==9 || c==34 || c==39|| c==37|| c==92)
         cout<<int(c)<<",";
-    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);%c}%c\";\n";
+    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);fclose(file);function2();%c}%c\";\n";
     cout<<"fprintf(file,s,";
     //cout << ASCII \t \n \"
-    for(auto c:content)if(c==10 || c==9 || c==34 || c==39)
+    for(auto c:content)if(c==10 || c==9 || c==34 || c==39|| c==37|| c==92)
         cout<<int(c)<<",";
-    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);\n";
+    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);fclose(file);function2();\n";
     cout<<"}\n";
 }
--- my_quine.c
+++ my_quine.c
@@ -4,6 +4,6 @@ void function2() {}
 int main()
 {
 function1();FILE *file=fopen("tmp","w");
-char *s="#include <stdio.h>%cvoid function1() {}%cvoid function2() {}%cint main()%c{%cfunction1();FILE *file=fopen(%ctmp%c,%cw%c);%cchar *s=%c%s%c;%cfprintf(file,s,10,10,10,10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);%c}%c";
-fprintf(file,s,10,10,10,10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);
+char *s="#include <stdio.h>%cvoid function1() {}%cvoid function2() {}%cint main()%c{%cfunction1();FILE *file=fopen(%ctmp%c,%cw%c);%cchar *s=%c%s%c;%cfprintf(file,s,10,10,10,10,10,34,34,34,34,10,34,s,34,10,10,10);fclose(file);function2();%c}%c";
+fprintf(file,s,10,10,10,10,10,34,34,34,34,10,34,s,34,10,10,10);fclose(file);function2();
 }
