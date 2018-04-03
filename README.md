edc7fc2f90a624e19c39bb175bb8adcb0c710233
kzoacn <kzoacn@outlook.com>
2018-04-03 19:41:10 +0800

    quine

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Quine-embeder
+
+Quine-embeder is a cpp code which can embed your C code into a C language quine program.
+
+## build and run
+
+Build:
+
+``` bash
+g++ embeder.cpp -o embeder -std=c++11
+```
+
+Run:
+``` bash
+./embeder < C.in > my_quine.c
+gcc my_quine.c -o my_quine
+./my_quine 
+diff my_quine.c tmp
+```
+Now `my_quine.c` is a quine which output itself to file `tmp`
+
+Attention. The code in `C.in` have to look like this
+``` C
+/*your code*/
+void function1(){/*your code*/}
+void function2(){/*your code*/}
+```
+where `function1` will be called before quine output and `function2` will be called after.
---
+++ embeder.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<char>content;
+string fix(char c){
+    if(c==10)return "%c";
+    if(c==9)return "%c";
+    if(c==34)return "%c";
+    if(c==39)return "%c";
+    return string(1,c);
+}
+int main(){
+    char ch;
+    while((ch=cin.get())!=EOF){
+        content.push_back(ch);
+        cout<<ch;
+    }
+    cout<<"int main()\n";
+    cout<<"{\n";
+    cout<<"function1();FILE *file=fopen(\"tmp\",\"w\");\n";
+    cout<<"char *s=\"";
+    //cout << fixed C.in
+    for(auto c:content)
+        cout<<fix(c);
+    cout<<"int main()%c{%cfunction1();FILE *file=fopen(%ctmp%c,%cw%c);%cchar *s=%c%s%c;%cfprintf(file,s,";
+    //cout << ASCII \t \n \"
+    for(auto c:content)if(c==10 || c==9 || c==34 || c==39)
+        cout<<int(c)<<",";
+    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);%c}%c\";\n";
+    cout<<"fprintf(file,s,";
+    //cout << ASCII \t \n \"
+    for(auto c:content)if(c==10 || c==9 || c==34 || c==39)
+        cout<<int(c)<<",";
+    cout<<"10,10,34,34,34,34,10,34,s,34,10,10,10);function2();fclose(file);\n";
+    cout<<"}\n";
+}
